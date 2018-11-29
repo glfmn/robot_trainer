e6389ca5b84909c0e5c1c7f0ab08e1981a237eac
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-11-29 17:07:57 -0500

    Add instantiation for all gear types. Add gear boxes sprites

--- Assets/Scripts/Gear Creation/CreateGear40.cs
+++ Assets/Scripts/Gear Creation/CreateGear40.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateObject : MonoBehaviour
+public class CreateGear40 : MonoBehaviour
 {
 
     public GameObject objectToInstantiate;
     private GameObject myCurrentObject;
+
     // Use this for initialization
     void Start()
     {
@@ -18,34 +19,31 @@ public class CreateObject : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
                                                                           Input.mousePosition.y, 10.0f));
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-   
+
             if (hit.collider)
             {
-                Debug.Log("tag: " + tag);
-                Debug.Log("Position: " + mousePos); 
-
-                if (hit.collider.tag == "Spawn")
+                if (hit.collider.tag == "Spawn40")
                 {
-                    Debug.Log("tag: " + tag); 
                     myCurrentObject = Instantiate
-                                      (objectToInstantiate, 
+                                      (objectToInstantiate,
                                        new Vector3(mousePos.x, mousePos.y + 20.0f, 0.0f), Quaternion.identity);
-                                      
+
                 }
             }
 
-                else
-                {
-                    Debug.Log("nothing happened."); 
-                }
-
+            else
+            {
+                Debug.Log("nothing happened.");
             }
 
         }
+
     }
+}
+
 
 
---
+++ Assets/Scripts/Gear Creation/CreateGear8.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGear8 : MonoBehaviour {
+
+    public GameObject objectToInstantiate;
+    private GameObject myCurrentObject;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                                                                          Input.mousePosition.y, 10.0f));
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider)
+            {
+                if (hit.collider.tag == "Spawn8")
+                {
+                    myCurrentObject = Instantiate
+                                      (objectToInstantiate,
+                                       new Vector3(mousePos.x, mousePos.y + 20.0f, 0.0f), Quaternion.identity);
+
+                }
+            }
+
+            else
+            {
+                Debug.Log("nothing happened.");
+            }
+
+        }
+
+    }
+}
