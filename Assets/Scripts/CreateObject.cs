ab38da1ac1bb3fbf0346a37f5721ca5de59b0e7c
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-11-29 11:12:56 -0500

    Fix issue with gear creation.

--- Assets/Scripts/CreateObject.cs
+++ Assets/Scripts/CreateObject.cs
@@ -14,21 +14,34 @@ public class CreateObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if(Input.GetMouseButtonDown(0)){
-            Debug.Log("Nice.");
-            Vector3 p = Camera.main.ScreenToWorldPoint
-                          (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-            myCurrentObject = Instantiate
-                (objectToInstantiate, Camera.main.ScreenToWorldPoint(new Vector3(p.x - 27.1f, p.y + 13.5f, 0.0f)),Quaternion.identity);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x + 21.8f, mousePos.y + .8f);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            //if (hit.collider.tag == "Respawn")
+            //{
+            //    Debug.Log("hit");
+            //    Vector3 p = Camera.main.ScreenToWorldPoint
+            //          (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            //    myCurrentObject = Instantiate
+            //        (objectToInstantiate, Camera.main.ScreenToWorldPoint
+            //         (new Vector3(p.x - 27.1f, p.y + 13.5f, 0.0f)), Quaternion.identity);
+            //}
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                hit.collider.attachedRigidbody.AddForce(Vector2.up);
+            }
+            Debug.Log(mousePos2D); 
+                Debug.Log("Nice.");
+
+          
      }
         if(Input.GetMouseButton(0) && myCurrentObject){
-            Debug.Log("Dragging");
-
             myCurrentObject.transform.position =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
      }
  
-        if(Input.GetMouseButtonUp(0) && myCurrentObject){
-            Debug.Log("letting go."); 
-
+        if(Input.GetMouseButtonUp(0) && myCurrentObject)
             myCurrentObject = null;
    
         }
@@ -36,5 +49,4 @@ public class CreateObject : MonoBehaviour {
     }
 		
 	
-}
 
