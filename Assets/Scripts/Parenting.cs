ca66e24e26c009768690be679f1a72d477016c8f
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-11-29 19:16:48 -0500

    Add parenting script.
    Object parents itself to the left most object.

---
+++ Assets/Scripts/Parenting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parenting : MonoBehaviour {
+
+    private GameObject child;
+    //private GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Contains("Gear"))
+        {
+            Debug.Log("setting parent!");
+            other.transform.parent = transform;
+        }
+
+        else
+        {
+            Debug.Log("nothing happening."); 
+        }
+    }
+}
