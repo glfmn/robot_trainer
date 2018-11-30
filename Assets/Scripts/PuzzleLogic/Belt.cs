b5550cfbe306fe0810d070a772c82fa35e52ee90
Gwen Lofman <Gwen@Lofman.io>
2018-11-29 19:27:03 -0500

    Update belt to fit a line between gears

--- Assets/Scripts/PuzzleLogic/Belt.cs
+++ Assets/Scripts/PuzzleLogic/Belt.cs
@@ -35,11 +35,28 @@ public class Belt : PuzzleElement {
         Vector3 d = pos_driven - pos_driver;
         Vector3 normal = new Vector3(-d.y, d.x, 0f);
         normal.Normalize();
-        Vector3 c_top = 1.5f * pos_driver + r_driver*normal;
-        Vector3 c_bot = 1.5f * pos_driven - r_driver*normal;
+        Vector3 d_top = ((pos_driver + r_driver*normal) - (pos_driven + r_driven*normal));
+        Vector3 c_top = pos_driver - 0.5f * d_top;
+        Vector3 d_bot = ((pos_driver - r_driver*normal) - (pos_driven - r_driven*normal));
+        Vector3 c_bot = pos_driver - 0.5f * d_bot;
 
         topBelt.transform.position = c_top;
         bottomBelt.transform.position = c_bot;
+
+        Vector3 n_bot = new Vector3(-d_bot.y, d_bot.x, 0f);
+        n_bot.Normalize();
+
+        Vector3 n_top = new Vector3(-d_top.y, d_top.x, 0f);
+        n_top.Normalize();
+
+        float z_top = Mathf.Acos(Vector3.Dot(Vector3.zero, n_top));
+        float z_bot = -Mathf.PI / 4f + Mathf.Acos(Vector3.Dot(Vector3.zero, n_bot));
+
+        topBelt.transform.rotation = new Quaternion(0f, 0f, z_top, 1f);
+        bottomBelt.transform.rotation = new Quaternion(0f, 0f, z_bot, 1f);
+
+        topBelt.transform.localScale = new Vector3(topBelt.transform.localScale.x, d_top.magnitude, 1f);
+        bottomBelt.transform.localScale = new Vector3(bottomBelt.transform.localScale.x, d_bot.magnitude, 1f);
     }
 
     public override float AngularVelocity () {
