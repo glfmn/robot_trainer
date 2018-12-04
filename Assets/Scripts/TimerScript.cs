d2ceeaee57d50954b7eea8df568392ac8e04fa7a
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-12-04 11:30:33 -0500

    Set PuzzleLogic to inactive to work in itch.io

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TimerScript : MonoBehaviour {
 
-    private GameObject left, right, linput, rinput;
+    private GameObject left, right, linput, rinput, puzzlelogic;
 
 	// Use this for initialization
 	void Start () {
@@ -12,13 +12,15 @@ public class TimerScript : MonoBehaviour {
         right = GameObject.Find("RightOutput");
         linput = GameObject.Find("LeftInput");
         rinput = GameObject.Find("RightInput");
+        puzzlelogic = GameObject.Find("PuzzleLogic"); 
 
         left.GetComponent<OutputGear>().enabled = false; 
         right.GetComponent<OutputGear>().enabled = false;
-        left.GetComponent<Parenting>().enabled = false;
-        right.GetComponent<Parenting>().enabled = false;
+        //left.GetComponent<Parenting>().enabled = false;
+        //right.GetComponent<Parenting>().enabled = false;
         linput.GetComponent<InputGear>().enabled = false;
         rinput.GetComponent<InputGear>().enabled = false;
+        puzzlelogic.SetActive(false); 
 
     }
 
@@ -32,35 +34,39 @@ public class TimerScript : MonoBehaviour {
 
         if (Input.GetKeyDown("space"))
         {
-            StartCoroutine(Activation(gameObject.GetComponent<RotateThis>().rotations));
-            Debug.Log("turning back on.");
-            Debug.Log("rotations: " + gameObject.GetComponent<RotateThis>().rotations); 
+            left.GetComponent<OutputGear>().enabled = true;
+            right.GetComponent<OutputGear>().enabled = true;
+            linput.GetComponent<InputGear>().enabled = true;
+            rinput.GetComponent<InputGear>().enabled = true;
+            puzzlelogic.SetActive(true); 
+            //left.GetComponent<Parenting>().enabled = true;
+            //right.GetComponent<Parenting>().enabled = true;
         }
 
     }
 
-    IEnumerator Activation(int num)
-    {
-        left.GetComponent<OutputGear>().enabled = true;
-        right.GetComponent<OutputGear>().enabled = true; 
-        linput.GetComponent<InputGear>().enabled = true;
-        rinput.GetComponent<InputGear>().enabled = true;
-        left.GetComponent<Parenting>().enabled = true;
-        right.GetComponent<Parenting>().enabled = true;
+    //IEnumerator Activation(int num)
+    //{
+    //    left.GetComponent<OutputGear>().enabled = true;
+    //    right.GetComponent<OutputGear>().enabled = true; 
+    //    linput.GetComponent<InputGear>().enabled = true;
+    //    rinput.GetComponent<InputGear>().enabled = true;
+    //    left.GetComponent<Parenting>().enabled = true;
+    //    right.GetComponent<Parenting>().enabled = true;
 
-        yield return new WaitForSeconds(num);
+    //    yield return new WaitForSeconds(num);
 
-        left.GetComponent<OutputGear>().enabled = false;
-        right.GetComponent<OutputGear>().enabled = false;
-        linput.GetComponent<InputGear>().enabled = false;
-        rinput.GetComponent<InputGear>().enabled = false;
-        left.GetComponent<Parenting>().enabled = false;
-        right.GetComponent<Parenting>().enabled = false;
+    //    left.GetComponent<OutputGear>().enabled = false;
+    //    right.GetComponent<OutputGear>().enabled = false;
+    //    linput.GetComponent<InputGear>().enabled = false;
+    //    rinput.GetComponent<InputGear>().enabled = false;
+    //    left.GetComponent<Parenting>().enabled = false;
+    //    right.GetComponent<Parenting>().enabled = false;
 
-        right.GetComponent<OutputGear>().outputVelocity = 0;
-        left.GetComponent<OutputGear>().outputVelocity = 0;
+    //    right.GetComponent<OutputGear>().outputVelocity = 0;
+    //    left.GetComponent<OutputGear>().outputVelocity = 0;
 
-        GameObject.FindGameObjectsWithTag("Gear"); 
-    }
+    //    GameObject.FindGameObjectsWithTag("Gear"); 
+    //}
 
 }
