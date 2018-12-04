20e8030ae72073b362d6314909444402e3963d4a
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-12-03 21:02:45 -0500

    Add splash menu scene and link to game scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+     
+public class LoadScene : MonoBehaviour
+
+{
+
+    public void NextScene()
+
+    {
+    
+        SceneManager.LoadScene("Scene0");
+
+    }
+
+}
--- Assets/Scripts/PuzzleLogic/InputGear.cs
+++ Assets/Scripts/PuzzleLogic/InputGear.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InputGear : PuzzleElement {
     public float size;
     public float input;
+    public int rotations = 0; 
 
     void Start () {
 
@@ -14,8 +15,48 @@ public class InputGear : PuzzleElement {
 
     }
 
-    void FixedUpdate () {
-        transform.Rotate(0, 0, input * Time.fixedDeltaTime);
+    void FixedUpdate()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                                                                          Input.mousePosition.y, 10.0f));
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider)
+            {
+                if (hit.collider.tag == "Input")
+                {
+                    Debug.Log("Rotating.");
+                    transform.Rotate(0, 0, 180);
+                    rotations++;
+                }
+
+            }
+
+            if (Input.GetKeyDown("space"))
+            {
+                StartCoroutine(MyCounter(gameObject.GetComponent<InputGear>().rotations));
+            }
+        }
+    }
+
+    IEnumerator MyCounter(int number)
+    {
+        float timePassed = 0;
+        while (timePassed < number)
+        {
+            Debug.Log("doing counter.:"); 
+            transform.Rotate(0, 0, input * Time.fixedDeltaTime);
+            /* wait one second per interval */
+            //yield return 0; 
+            //yield return new WaitForSeconds(1.0f); 
+          
+            timePassed += Time.fixedDeltaTime;
+            yield return null;
+        }
     }
 
     public override float AngularVelocity () {
